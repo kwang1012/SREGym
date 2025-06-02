8b135b127edbd68c614538004d820034c849a4b1
Saad Mohammad Rafid Pial <saadmrp222@gmail.com>
2025-06-02 23:23:28 +0600

    add formatter to log message

--- provisioner/utils/logger.py
+++ provisioner/utils/logger.py
@@ -1,3 +1,16 @@
 import logging
 
-logger = logging.getLogger("CloudlabProvisioner")
+# Create logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create console handler and set level
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# Add console handler to logger
+logger.addHandler(console_handler)
