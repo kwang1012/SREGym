f65d1ed26346362a33995248e5f7d08ec2c46486
Jackson Clark <jclark58@illinois.edu>
2025-06-13 12:22:24 -0500

    Added two more problems

--- srearena/conductor/problems/registry.py
+++ srearena/conductor/problems/registry.py
@@ -70,7 +70,15 @@ class ProblemRegistry:
             "resource_request_too_small": lambda: ResourceRequestTooSmall(
                 app_name="hotel_reservation", faulty_service="mongodb-rate"
             ),
-            "wrong_service_selector": lambda: WrongServiceSelector(app_name="astronomy_shop", faulty_service="frontend"),
+            "wrong_service_selector_astronomy_shop": lambda: WrongServiceSelector(
+                app_name="astronomy_shop", faulty_service="frontend"
+            ),
+            "wrong_service_selector_hotel_reservation": lambda: WrongServiceSelector(
+                app_name="hotel_reservation", faulty_service="frontend"
+            ),
+            "wrong_service_selector_social_network": lambda: WrongServiceSelector(
+                app_name="social_network", faulty_service=""
+            ),
             # "missing_service_astronomy_shop": lambda: MissingService(app_name="astronomy_shop", faulty_service="ad"),
             # K8S operator misoperation -> Refactor later, not sure if they're working
             # They will also need to be updated to the new problem format.
