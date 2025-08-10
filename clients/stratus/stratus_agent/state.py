c775e1b6e75bac018f80817cd3c5afc3f0bc844b
Yiming Su <yiming34@illinois.edu>
2025-08-10 15:21:30 -0500

    changing how the state is defined
    Signed-off-by: Yiming Su <yiming34@illinois.edu>

--- clients/stratus/stratus_agent/state.py
+++ clients/stratus/stratus_agent/state.py
@@ -7,13 +7,14 @@ class State(TypedDict):
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
-    workdir: str
-    curr_file: str
-    curr_line: int
+    # workdir: str
+    # curr_file: str
+    # curr_line: int
     # number or rounds used to finish assigned tasks
-    num_rounds: int
+    # num_rounds: int
+    num_steps: int
     # number of rounds used for rectifying submission
-    rec_submission_rounds: int
+    # rec_submission_rounds: int
     submitted: bool
-    submit_tried: bool
-    ans: dict
+    # submit_tried: bool
+    # ans: dict
