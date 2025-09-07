66d133f1f3747de400582f7c688800b3b96576c9
Saad Mohammad Rafid Pial <saadmrp222@gmail.com>
2025-09-07 07:01:31 +0600

    remove CompoundOracle

--- srearena/conductor/problems/read_error.py
+++ srearena/conductor/problems/read_error.py
@@ -32,11 +32,6 @@ class ReadError(Problem):
 
         self.localization_oracle = None
 
-        self.mitigation_oracle = CompoundedOracle(
-            self,
-            WorkloadOracle(problem=self, wrk_manager=self.app.wrk),
-        )
-
     # --------- Fault actions ----------
 
     @mark_fault_injected
