0d2c4034bc764dd06898014d7c98f617f559f7ee
lilygniedz <lilygn6@gmail.com>
2025-11-13 14:52:14 -0600

    Go.sh

--- tests/scripts/go.sh
+++ tests/scripts/go.sh
@@ -3,4 +3,5 @@ sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.24.1.linux-amd64.ta
 echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
 echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> $HOME/.bashrc
 sudo rm -f go1.24.1.linux-amd64.tar.gz
-sudo rm -f go1.24.1.linux-amd64.tar.gz.1
+sudo rm -f go1.24.1.linux-amd64.tar.gz.\
+
